7ea4cd160e00feba706c4acd083e4bb1bf793ea9
Vanderlei <vanderleijr2001@gmail.com>
2024-12-09 20:26:13 -0800

    Initial Commit

---
+++ final test/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  final test
+//
+//  Created by Vanderlei Junior on 12/9/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
